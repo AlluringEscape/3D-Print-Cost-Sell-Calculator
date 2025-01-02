3bcbad1f66da33574ecd3fab26dcb83e58e8c79b
Nick <N.j.linton25Gmail.com>
2025-01-02 17:03:10 -0600

    Name changed to 3D Print Cost/Sell Calculator

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ from settings import open_settings, open_printer_settings, open_material_setting
 
 #Initalize the main app window
 root = Tk()
-root.title("3D Printing Business Calculator")
+root.title("3D Print Cost/Sell Calculator")
 root.geometry("1600x1400")
 
+#Main Page widgets
+
+#Printer
+Label(root, text="Printer:").grid(row=0, column=0, sticky=W)
+printer_dropdown = ttk.Combobox(root, values=["Printer 1","Printer 2"])
+printer_dropdown.grid(row=0,    column=1)
+Button(root, text="Printer settings", command=open_printer_settings).gird(row=0, column=2)
+
+#
